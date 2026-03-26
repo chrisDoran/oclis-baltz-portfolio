1dc1a6c1340a97936f1be9001686631647f7bbad
chrisDoran <chris.thomas.doran@gmail.com>
2026-03-26 17:04:47 +1100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./app"; // lowercase to match the file name
-import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
