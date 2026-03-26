047010ba2f7f37abea80996b99d2ba87a29ba196
chrisDoran <chris.thomas.doran@gmail.com>
2026-03-26 18:52:42 +1100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 
-import logo from "./images/Logo.png";
+import logo from "/Logo.png";
 
 function App() {
   const [page, setPage] = useState("main");
